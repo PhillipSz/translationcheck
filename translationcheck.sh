3b35bf92cde238d43f6330e8e3a72d8721215573
Phillip Sz <phillip.szelat@gmail.com>
2014-11-29 20:50:21 +0100

    better code style

--- translationcheck.sh
+++ translationcheck.sh
@@ -3,8 +3,8 @@
 # Script to check for Launchpad Translation
 # Uncomment/comment which function you would like to use
 #
-# To do: 
-# - automaticly open in browser
+# Todo: - check if the input from launchpad really is a number; if not exit 1
+# 
 
 # curently only elementary os works!
 
@@ -18,16 +18,19 @@ openns="1" # set to 1 to open all strings that needs review in a browser, 0 to o
 name=("noise" "switchboard-plug-keyboard" "elementaryos" "snap-elementary" "audience" "slingshot" "switchboard-plug-pantheon-shell" "switchboard-plug-locale" "switchboard-plug-display" "switchboard-plug-applications" "scratch" "gala" "switchboard-plug-about" "pantheon-files" "switchboard-plug-notifications" "switchboard-plug-security-privacy" "switchboard" "maya" "wingpanel" "switchboard-plug-power" "appcenter" "pantheon-greeter" "euclide" "switchboard-plug-onlineaccounts" "pantheon-terminal" "granite" "maya" "noise" "pantheon-photos" "midori")
 namelenght="$[${#name[@]}-1]"
 
+# just to not be influenced by an env var
+shown="" 
+opened=""
+
 for i in $(seq 0 $namelenght)
 do
 
 	dw="$(wget -q -O- https://translations.launchpad.net/${name[$i]}/ | grep -A 30 ">$lang" | grep '<span class="sortkey">'| tail -n2)"
 
-	ut="$(echo "$dw" | head -n1 | egrep -o "[0-9]+")"
+	ut="$(echo "$dw" | head -n1 | egrep -o "[0-9]+")" # ut = untranslated
 
-	ns="$(echo "$dw" | tail -n1 | egrep -o "[0-9]+")"
+	ns="$(echo "$dw" | tail -n1 | egrep -o "[0-9]+")" # ns = needs review
 	
-
 	if [ "$ut" != "0" ]
 		then
 			echo "${name[$i]}:"
@@ -36,7 +39,7 @@ do
 		
 			if [ "$openut" == "1" ]
 				then 
-					eval "xdg-open https://translations.launchpad.net/${name[$i]}/ 2> /dev/null"
+					xdg-open https://translations.launchpad.net/${name[$i]}/ 2> /dev/null
 					opened="1"			
 			fi
 		else
@@ -47,7 +50,7 @@ do
 		then
 			if [ "$openns" == "1" ] && [ "$opened" != "1" ]
 				then 
-					eval "xdg-open https://translations.launchpad.net/${name[$i]}/ 2> /dev/null"
+					xdg-open https://translations.launchpad.net/${name[$i]}/ 2> /dev/null
 			fi
 
 			if [ "$shown" == "1" ]
