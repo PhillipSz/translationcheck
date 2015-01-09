2a343659d0dba015b2964a86140617a4a556bbf2
Phillip Sz <phillip.szelat@gmail.com>
2015-01-09 17:35:39 +0100

    translationcheck.bash: added ubuntu and elementary options

--- translationcheck.bash
+++ translationcheck.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Script to check for Launchpad Translation
-# Uncomment/comment which function you would like to use
 #
 # Todo: - ask user on frist run and then write a .config or use options like ./translationcheck -a -v -f
-#	- allow to check ubuntu and elementary
 # 
 # curently only elementary os apps and ubuntu-touch apps works!
+#
 
+lang="German" # change this to your needs, e.g.: "French" or "Greek"
 
-lang="German" # change to your need, e.g.: "French" or "Greek"
 openut="0" # set to 1 to open all untranslated apps in a browser, 0 to only show
 openns="0" # set to 1 to open all strings that needs review in a browser, 0 to only show
+
+checkubuntu="0" # set to 1 to check ubuntu apps, 0 to not do so 
+checkelementary="1" # set to 1 to check elementary apps, 0 to not do so
 
 nameselementary=("noise" "switchboard-plug-keyboard" "elementaryos" "snap-elementary" "audience" "slingshot" "switchboard-plug-pantheon-shell" "switchboard-plug-locale" "switchboard-plug-display" "switchboard-plug-applications" "scratch" "gala" "switchboard-plug-about" "pantheon-files" "switchboard-plug-notifications" "switchboard-plug-security-privacy" "switchboard" "maya" "wingpanel" "switchboard-plug-power" "appcenter" "pantheon-greeter" "euclide" "switchboard-plug-onlineaccounts" "pantheon-terminal" "granite" "maya" "noise" "pantheon-photos" "midori")
 namesubuntu=("ubuntu-system-settings" "ubuntu-rest-scopes" "music-app" "address-book-app" "webbrowser-app" "gallery-app" "ubuntu-clock-app" "dialer-app" "sudoku-app" "ubuntu-rssreader-app" "ubuntu-calendar-app" "ubuntu-weather-app" "reminders-app" "unity8" "messaging-app" "indicator-network" "unity-scope-click" "camera-app" "unity-scope-mediascanner" "ubuntu-system-settings-online-accounts" "curucu" "mediaplayer-app" "ubuntu-calculator-app" "notes-app" "unity-scope-scopes" "indicator-location" "telephony-service" "indicator-location")
@@ -21,12 +23,13 @@ checktranslations(){
 # start script
 # I use declare to be able to use a varibale as array name
 
-declare -n names="$1" # or use typeset, but it the same in bash
+declare -n names="$1" # or use typeset, but it the same in bash; # we need namesubuntu or nameselementary as input here
 
 # How many programms we would like to check? (-1 because array start with 0)
  
 namelength="$((${#names[@]} -1))"
-echo "$namelength"
+
+# echo "$namelength"
 
 # just to not be influenced by an env var
 shown="" 
@@ -40,12 +43,12 @@ for i in $(seq 0 $namelength); do
 
 	ns="$(echo "$dw" | tail -n1 | egrep -o "[0-9]+")" # ns = needs review
 	
-	if [[ "$ut" =~ [0-9]+ && "$ns" =~ [0-9]+ ]]; then 
+	if [[ "$ut" =~ [0-9]+ && "$ns" =~ [0-9]+ ]]; then # in case $ut or $ns = "" give an error
 		
 		if [[ "$ut" != "0" ]]; then
 			
 			echo "${names[$i]}:"
-			shown=1
+			shown="1"
 			echo "$ut untranslated"
 		
 			if [[ "$openut" == "1" ]]; then 
@@ -59,7 +62,7 @@ for i in $(seq 0 $namelength); do
    
 		if [[ "$ns" != "0" ]]; then
 
-			if [[ "$openns" == "1" ]] && [[ "$opened" != "1" ]]; then 
+			if [[ "$openns" == "1" && "$opened" != "1" ]]; then 
 				xdg-open https://translations.launchpad.net/${names[$i]}/ 2> /dev/null
 			fi
 
@@ -75,10 +78,18 @@ for i in $(seq 0 $namelength); do
 	
 	else
 		echo "We have a problem!"
-		echo "name = ${names[$i]}; ut = $ut; ns = $ns"
+		echo "Debug: name = ${names[$i]}; ut = $ut; ns = $ns; \$1 = $1"
 		exit 1
 fi
 done
 }
 
-checktranslations namesubuntu # just type namesubuntu or nameselementary here
+# Todo: make this part not so static!
+
+if [[ "$checkubuntu" == "1" ]]; then
+	checktranslations namesubuntu
+fi
+
+if [[ "$checkelementary" == "1" ]]; then
+	checktranslations nameselementary
+fi
