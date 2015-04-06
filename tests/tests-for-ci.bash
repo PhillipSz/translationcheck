aa9c435704c5a417aa0f0abad0af30a6256e562c
Phillip Sz <phillip.szelat@gmail.com>
2015-04-06 19:20:28 +0200

    ci: test a few languages

--- tests/tests-for-ci.bash
+++ tests/tests-for-ci.bash
@@ -3,5 +3,8 @@
 # test for translationcheck.bash
 
 bash translationcheck.bash -uel "german" > /dev/null || { echo "error" >&2; exit 1; }
+bash translationcheck.bash -uel "french" > /dev/null || { echo "error" >&2; exit 1; }
+bash translationcheck.bash -uel "Chinese (Simplified)" > /dev/null || { echo "error" >&2; exit 1; }
+bash translationcheck.bash -uel "English (United Kingdom)" > /dev/null || { echo "error" >&2; exit 1; }
 echo $BASH_VERSION
 echo "works!"
--- tests/tests.bash
+++ tests/tests.bash
@@ -2,5 +2,9 @@
 #
 # test for translationcheck.bash
 
-bash ../translationcheck.bash -uel "german" > /dev/null || { echo "error" >&2; exit 1; }
+bash ~/translationcheck/translationcheck.bash -uel "german" > /dev/null || { echo "error" >&2; exit 1; }
+bash ~/translationcheck/translationcheck.bash -uel "french" > /dev/null || { echo "error" >&2; exit 1; }
+bash ~/translationcheck/translationcheck.bash -uel "Chinese (Simplified)" > /dev/null || { echo "error" >&2; exit 1; }
+bash ~/translationcheck/translationcheck.bash -uel "English (United Kingdom)" > /dev/null || { echo "error" >&2; exit 1; }
+echo $BASH_VERSION
 echo "works!"
