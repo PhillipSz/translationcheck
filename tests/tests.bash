0057529c313931a933b8dde9275ee6365c59b5b5
Phillip Sz <phillip.szelat@gmail.com>
2015-05-03 13:25:28 +0200

    added better .gitignore

--- tests/tests.bash
+++ tests/tests.bash
@@ -6,5 +6,6 @@ bash ~/translationcheck/translationcheck.bash -uel "german" > /dev/null || { ech
 bash ~/translationcheck/translationcheck.bash -uel "french" > /dev/null || { echo "error" >&2; exit 1; }
 bash ~/translationcheck/translationcheck.bash -uel "Chinese (Simplified)" > /dev/null || { echo "error" >&2; exit 1; }
 bash ~/translationcheck/translationcheck.bash -uel "English (United Kingdom)" > /dev/null || { echo "error" >&2; exit 1; }
+bash ~/translationcheck/translationcheck.bash -uel "Assamese" > /dev/null || { echo "error" >&2; exit 1; }
 echo $BASH_VERSION
 echo "works!"
