d13b33698d399d047b87a02381d0539a983af5f6
Phillip Sz <phillip.szelat@gmail.com>
2015-11-20 21:55:09 +0100

    tests: only use one file for both cases

--- tests/tests.bash
+++ tests/tests.bash
@@ -3,14 +3,26 @@
 # test for translationcheck.bash
 
 tests (){
-	~/translationcheck/translationcheck.bash -cuesl "german" > /dev/null || echo "error" # it probably never hit this echo
-	~/translationcheck/translationcheck.bash -cuesl "french" > /dev/null || echo "error"
-	~/translationcheck/translationcheck.bash -cuesl "Chinese (Simplified)" > /dev/null || echo "error"
-	~/translationcheck/translationcheck.bash -cuesl "English (United Kingdom)" > /dev/null ||  echo "error"
+	$HOME/translationcheck/translationcheck.bash -cuesl "german" > /dev/null || echo "error" # it probably never hit this echo
+	$HOME/translationcheck/translationcheck.bash -cuesl "french" > /dev/null || echo "error"
+	$HOME/translationcheck/translationcheck.bash -cuesl "Chinese (Simplified)" > /dev/null || echo "error"
+	$HOME/translationcheck/translationcheck.bash -cuesl "English (United Kingdom)" > /dev/null ||  echo "error"
 }
 
-time tests
+
+test2 (){
+	translationcheck.bash -cuesl "german" > /dev/null || echo "error" # it probably never hit this echo
+	translationcheck.bash -cuesl "french" > /dev/null || echo "error"
+	translationcheck.bash -cuesl "Chinese (Simplified)" > /dev/null || echo "error"
+	translationcheck.bash -cuesl "English (United Kingdom)" > /dev/null ||  echo "error"
+}
+
+if [[ -e "$HOME/translationcheck/translationcheck.bash" ]];then
+	time tests
+elif [[ -e translationcheck.bash ]];then
+	time test2
+fi
 
 echo $BASH_VERSION
 
-echo "Read the output from the script, as still script will always exit with 0!"
+echo "Read the output from the script, as it will always exit with 0!"
