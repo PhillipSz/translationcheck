6664762c65c5719096645b12e38c51d24edb5268
Phillip Sz <phillip.szelat@gmail.com>
2016-01-05 19:35:29 +0100

    add test for updateapplists()
    Also:
    - rewrite the test a bit
    - let main() write to file, so we can test updateapplists()
    - better error handling
    - better import of os

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@
 #
 # Just use "python -m unittest discover -v" to see what happens
 #
-
+'''Tests for translationcheck'''
 import unittest
 # ugly hack to import translationcheck
 # from http://stackoverflow.com/questions/4542352/import-from-sibling-directory
@@ -12,7 +12,40 @@ os.sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.ge
 
 import translationcheck
 
-# TODO: Add a test for parseargs(), updateapplists(), printit() if possible
+# TODO: Add a test for parseargs(), printit() if possible
+
+class updateapplists(unittest.TestCase):
+    '''Tests for updateapplists'''
+    def test_updateapplists(self):
+        '''Test if we get an empty dict back (don't know if that test is useful) '''
+        dict_of_apps = translationcheck.updateapplists()
+        self.assertTrue(dict_of_apps)
+        self.assertTrue(isinstance(dict_of_apps, dict))
+
+    def test_check_if_false_app_elementary(self):
+        '''Test if the function does gives us an elementary app that is not translatable'''
+        dict_of_apps = translationcheck.updateapplists()
+
+        for project, apps in dict_of_apps.items():
+            if project == 'elementary':
+                # test if the first and last app in the list is there.
+                # this should test the regex
+                self.assertFalse('contractor' in apps)
+                self.assertFalse('wingpanel-indicator-a11y' in apps)
+                self.assertTrue('scratch' in apps)
+                self.assertTrue('switchboard' in apps)
+
+    def test_check_if_false_app_unityscopes(self):
+        '''Test if the function does gives us a unity scope that is not translatable'''
+        dict_of_apps = translationcheck.updateapplists()
+
+        for project, apps in dict_of_apps.items():
+            if project == 'unity-scopes':
+                self.assertTrue('unity-scope-isgd' in apps) # it's called: "Unity is.gd Scope" that's why we should test that
+                self.assertTrue('unity-scope-zotero' in apps)
+                self.assertTrue('unity-scope-ubuntushop' in apps)
+                self.assertFalse('unity-scope-snappy' in apps)
+                self.assertFalse('unity-scope-piratebay' in apps)
 
 class getappsTestCase(unittest.TestCase):
     '''Tests for getapps.'''
@@ -20,42 +53,54 @@ class getappsTestCase(unittest.TestCase):
     def test_get_apps(self):
         '''Test the function to parse the apps to check'''
 
-        self.results = {'elementary': {}, 'ubuntu': {}, 'unity-scopes': {}}
-        self.results = translationcheck.getapps(self.results)
-        self.results_wanted = {'ubuntu': {'ubuntu-terminal-app': [], 'unity-scope-scopes': [], 'ubuntu-rssreader-app': [], 'webbrowser-app': [], 'unity8': [], 'messaging-app': [], 'reminders-app': [], 'ubuntu-keyboard': [], 'help-app': [], 'indicator-location': [], 'dialer-app': [], 'camera-app': [], 'ubuntu-clock-app': [], 'ubuntu-weather-app': [], 'software-center-agent': [], 'today-scope': [], 'twitter-scope': [], 'music-app': [], 'indicator-bluetooth': [], 'ciborium': [], 'ubuntu-rest-scopes': [], 'ubuntu-docviewer-app': [], 'telegram-app': [], 'telephony-service': [], 'address-book-app': [], 'location-service': [], 'ubuntu-calendar-app': [], 'ubuntu-filemanager-app': [], 'unity-scope-mediascanner': [], 'mediaplayer-app': [], 'indicator-network': [], 'sudoku-app': [], 'notes-app': [], 'gallery-app': [], 'ubuntu-system-settings-online-accounts': [], 'ubuntu-ui-toolkit': [], 'ubuntu-calculator-app': []}, 'elementary': {'pantheon-mail': [], 'switchboard-plug-keyboard': [], 'switchboard-plug-locale': [], 'switchboard-plug-onlineaccounts': [], 'wingpanel-indicator-datetime': [], 'switchboard-plug-power': [], 'granite': [], 'appcenter': [], 'switchboard-plug-useraccounts': [], 'gala': [], 'switchboard-plug-about': [], 'switchboard-plug-networking': [], 'wingpanel-indicator-notifications': [], 'switchboard-plug-applications': [], 'wingpanel-indicator-power': [], 'switchboard-plug-a11y': [], 'audience': [], 'wingpanel-indicator-network': [], 'pantheon-calculator': [], 'snap-elementary': [], 'midori': [], 'switchboard-plug-mouse-touchpad': [], 'screenshot-tool': [], 'wingpanel-indicator-bluetooth': [], 'switchboard-plug-parental-controls': [], 'switchboard-plug-sharing': [], 'wingpanel-indicator-sound': [], 'wingpanel-indicator-session': [], 'switchboard-plug-pantheon-shell': [], 'pantheon-photos': [], 'pantheon-greeter': [], 'wingpanel': [], 'scratch': [], 'maya': [], 'elementaryos': [], 'capnet-assist': [], 'switchboard-plug-datetime': [], 'plank': [], 'switchboard-plug-notifications': [], 'switchboard-plug-security-privacy': [], 'wingpanel-indicator-keyboard': [], 'switchboard-plug-display': [], 'slingshot': [], 'pantheon-terminal': [], 'pantheon-files': [], 'switchboard': [], 'noise': [], 'switchboard-plug-printers': []}, 'unity-scopes': {'unity-scope-audacious': [], 'unity-scope-yelp': [], 'unity-scope-virtualbox': [], 'unity-scope-etsy': [], 'unity-scope-sshsearch': [], 'unity-scope-stackexchange': [], 'unity-scope-ebay': [], 'unity-scope-pubmed': [], 'unity-scope-firefoxbookmarks': [], 'unity-scope-medicines': [], 'unity-scope-wikipedia': [], 'unity-scope-nullege': [], 'unity-scope-zeitgeistwebhistory': [], 'unity-scope-chromiumbookmarks': [], 'unity-scope-songsterr': [], 'unity-scope-jstor': [], 'unity-scope-clementine': [], 'unity-scope-mediascanner': [], 'unity-scope-deviantart': [], 'unity-scope-isgd': [], 'unity-scope-ubuntushop': [], 'unity-scope-zotero': [], 'unity-scope-calculator': [], 'unity-scope-tomboy': [], 'unity-scope-europeana': [], 'unity-scope-remmina': [], 'unity-scope-yelpplaces': [], 'unity-scope-musique': [], 'unity-scope-gmusicbrowser': [], 'unity-scope-github': [], 'unity-scope-tumblr': [], 'unity-scope-guayadeque': [], 'unity-scope-soundcloud': [], 'unity-scope-manpages': [], 'unity-scope-evolution': [], 'unity-scope-texdoc': [], 'unity-scope-yahoostock': [], 'unity-scope-googlescholar': [], 'unity-scope-askubuntu': [], 'unity-scope-sciencedirect': [], 'unity-scope-gallica': [], 'unity-scope-foursquare': [], 'unity-scope-calibre': [], 'unity-scope-devhelp': [], 'unity-scope-pypi': [], 'unity-scope-recipepuppy': [], 'unity-scope-launchpad': [], 'unity-scope-scopes': [], 'unity-scope-googlenews': [], 'unity-scope-dribbble': [], 'unity-scope-grooveshark': [], 'unity-scope-click': [], 'unity-scope-songkick': [], 'unity-scope-openclipart': [], 'unity-scope-gourmet': [], 'unity-scope-imdb': [], 'unity-scope-wikispecies': [], 'unity-scope-openweathermap': [], 'unity-scope-phpdoc': [], 'unity-scope-googlebooks': [], 'unity-scope-reddit': [], 'unity-scope-colourlovers': []}}
-        self.assertEqual(self.results, self.results_wanted)
+        results = {'elementary': {}, 'ubuntu': {}, 'unity-scopes': {}}
+        results = translationcheck.getapps(results)
+        self.assertTrue('elementary' and 'ubuntu' and 'unity-scopes' in results)
+
+        for project, apps in results.items():
+            if project == 'ubuntu':
+                self.assertTrue('ubuntu-system-settings-online-accounts' in apps)
+                self.assertTrue('unity8' in apps)
+            if project == 'elementary':
+                self.assertTrue('switchboard-plug-parental-controls' in apps)
+                self.assertTrue('plank' in apps)
+            if project == 'unity-scopes':
+                self.assertTrue('unity-scope-isgd' in apps)
+                self.assertTrue('unity-scope-zeitgeistwebhistory' in apps)
+                self.assertTrue('unity-scope-zotero' in apps)
 
 class getresultsTestCase(unittest.TestCase):
-    '''Tests for getresults.
+    '''Tests for getresults().
        I just choose these at random and with the hope that they don't change that much.'''
 
+    # we only test for 0 and error and elnf here, as the rest can change fast
     def test_getresults_elementary(self):
         '''Test the function to parse the webpages for elementary'''
 
-        self.results_e_de = translationcheck.getresults("pantheon-calculator", "German")
-        self.assertEqual(self.results_e_de, ('0', '0'))
-        self.results_e_de_error = translationcheck.getresults("this-is-not-an-app-:)", "German")
-        self.assertEqual(self.results_e_de_error, ('error', 'error'))
-        self.results_e_nor = translationcheck.getresults("pantheon-calculator", "Norwegian Bokmal")
-        self.assertEqual(self.results_e_nor, ('0', '0'))
-        self.results_e_ch = translationcheck.getresults("pantheon-calculator", "Chinese (Simplified)")
-        self.assertEqual(self.results_e_ch, ('0', '0'))
+        results_e_de = translationcheck.getresults("pantheon-calculator", "German")
+        self.assertEqual(results_e_de, ('0', '0'))
+        results_e_de_error = translationcheck.getresults("this-is-not-an-app-:)", "German")
+        self.assertEqual(results_e_de_error, ('error', 'error'))
+        results_e_nor = translationcheck.getresults("pantheon-calculator", "Norwegian Bokmal")
+        self.assertEqual(results_e_nor, ('0', '0'))
+        results_e_ch = translationcheck.getresults("pantheon-calculator", "Chinese (Simplified)")
+        self.assertEqual(results_e_ch, ('0', '0'))
 
     def test_getresults_unityscopes(self):
         '''Test the function to parse the webpages for unity-scopes'''
 
-        self.results_us_de = translationcheck.getresults("unity-scope-remmina", "German")
-        self.assertEqual(self.results_us_de, ('0', '0'))
-        self.results_us_ch = translationcheck.getresults("unity-scope-remmina", "Chinese (Simplified)")
-        self.assertEqual(self.results_us_ch, ('lnf', 'lnf'))
+        results_us_de = translationcheck.getresults("unity-scope-remmina", "German")
+        self.assertEqual(results_us_de, ('0', '0'))
+        results_us_ch = translationcheck.getresults("unity-scope-remmina", "Chinese (Simplified)")
+        self.assertEqual(results_us_ch, ('lnf', 'lnf'))
 
     def test_getresults_ubuntu(self):
         '''Test the function to parse the webpages for ubuntu'''
 
-        self.results_u_de = translationcheck.getresults("ubuntu-keyboard", "German")
-        self.assertEqual(self.results_u_de, ('0', '0'))
-        self.results_u_uk = translationcheck.getresults("ubuntu-keyboard", "English (United Kingdom)")
-        self.assertEqual(self.results_u_uk, ('0', '0'))
+        results_u_de = translationcheck.getresults("ubuntu-keyboard", "German")
+        self.assertEqual(results_u_de, ('0', '0'))
+        results_u_uk = translationcheck.getresults("ubuntu-keyboard", "English (United Kingdom)")
+        self.assertEqual(results_u_uk, ('0', '0'))
 
 if __name__ == '__main__':
     unittest.main()
